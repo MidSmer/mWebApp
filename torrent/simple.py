5ffbd9a360a0633a046c50877aa55919d2cf2147
MidSmer <3w.zhong.qiang@gmail.com>
2016-01-30 10:35:54 +0800

    空格

--- torrent/simple.py
+++ torrent/simple.py
@@ -6,11 +6,11 @@ from .decorator import login_required
 @app.route('/get-torrent', methods=['GET', 'POST'])
 @login_required
 def get_torrent():
-	if request.method == 'GET':
-		return render_template('get-torrent.html')
-	else:
-		result = change.magnet2torrent(request.form['magnet'])
-		if 200 != result['status']:
-			return result['message']
+    if request.method == 'GET':
+        return render_template('get-torrent.html')
+    else:
+        result = change.magnet2torrent(request.form['magnet'])
+        if 200 != result['status']:
+            return result['message']
 
-		return Response(result['data'], headers={'Content-Type':'application/octet-stream','Content-Disposition':'filename=' + result['name'] + '.torrent'})
+        return Response(result['data'], headers={'Content-Type':'application/octet-stream','Content-Disposition':'filename=' + result['name'] + '.torrent'})
--- torrent/view.py
+++ torrent/view.py
@@ -3,15 +3,15 @@ from torrent import app
 
 @app.route('/')
 def index_view():
-	return render_template('view.html')
+    return render_template('view.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login_view():
-	if request.method == 'POST':
-		if 'mweb' == request.form['name']:
-			session['userName'] = request.form['name']
+    if request.method == 'POST':
+        if 'mweb' == request.form['name']:
+            session['userName'] = request.form['name']
 
-	if 'userName' in session:
-		return redirect(url_for('get_torrent'))
+    if 'userName' in session:
+        return redirect(url_for('get_torrent'))
 
-	return render_template('login.html')
+    return render_template('login.html')
